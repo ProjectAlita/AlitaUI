323f1ad69a7937ebe5988bef7e8102f6f1639005
Kevin_Cai <Kevin_Cai@epam.com>
2023-11-13 10:46:55 +0800

    refactor: propmt related components refactory

---
+++ src/pages/PromptDetail/Common.jsx
@@ -0,0 +1,104 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Grid, TextField, Avatar, Typography } from "@mui/material";
+import { PROMPT_PAYLOAD_KEY } from "@/common/constants.js";
+import { actions as promptSliceActions } from "@/reducers/prompts";
+import { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { contextResolver } from "@/common/utils";
+
+export const StyledGridContainer = styled(Grid)(() => ({
+  padding: 0,
+}));
+
+export const LeftGridItem = styled(Grid)(() => ({
+  position: "relative",
+  padding: "0 0.75rem",
+}));
+
+export const RrightGridItem = styled(Grid)(() => ({
+  padding: "0 0.75rem",
+}));
+
+export const StyledInput = styled(TextField)(() => ({
+  marginBottom: "0.75rem",
+  "& .MuiFormLabel-root": {
+    fontSize: "0.875rem",
+    lineHeight: "1.375rem",
+    top: "-0.25rem",
+    left: "0.75rem",
+  },
+  "& .MuiInputBase-root": {
+    padding: "1rem 0.75rem",
+    marginTop: "0",
+  },
+}));
+
+export const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  width: "1.75rem",
+  height: "1.75rem",
+  display: "flex",
+  flex: "0 0 1.75rem",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: theme.palette.secondary.main,
+}));
+
+export const TabBarItems = styled("div")(() => ({
+  position: "absolute",
+  top: "-3.7rem",
+  right: "0.5rem",
+}));
+
+export const SelectLabel = styled(Typography)(() => ({
+  display: "inline-block",
+}));
+
+export const StyledInputEnhancer = (props) => {
+    const dispatch = useDispatch();
+    const { payloadkey, label } = props;
+    const { currentPrompt } = useSelector((state) => state.prompts);
+    let theValue = currentPrompt && currentPrompt[payloadkey];
+    if (payloadkey === PROMPT_PAYLOAD_KEY.variables) {
+      theValue = theValue.find((item) => item.key === label).value;
+    }
+    const [value, setValue] = useState(
+      payloadkey === PROMPT_PAYLOAD_KEY.tags
+        ? theValue?.map((tag) => tag?.tag).join(",")
+        : theValue
+    );
+    const handlers = {
+      onBlur: useCallback((event) => {
+        if (payloadkey === PROMPT_PAYLOAD_KEY.variables) return;
+        const { target } = event;
+        const inputValue = target?.value;
+        if (payloadkey === PROMPT_PAYLOAD_KEY.context) {
+          dispatch(
+            promptSliceActions.updateCurrentPromptData({
+              key: PROMPT_PAYLOAD_KEY.variables,
+              data: contextResolver(inputValue).map((variable) => {
+                return {
+                  key: variable,
+                  value: "",
+                };
+              }),
+            })
+          );
+        } else {
+          dispatch(
+            promptSliceActions.updateCurrentPromptData({
+              key: payloadkey,
+              data:
+                payloadkey === PROMPT_PAYLOAD_KEY.tags
+                  ? target?.value?.split(",")
+                  : target?.value,
+            })
+          );
+        }
+      }, []),
+      onChange: useCallback((event) => {
+        const { target } = event;
+        setValue(target?.value);
+      }),
+    };
+    return <StyledInput value={value} {...handlers} {...props} />;
+  };
--- src/pages/PromptDetail/EditPromptDetail.jsx
+++ src/pages/PromptDetail/EditPromptDetail.jsx
@@ -1,231 +1,174 @@
-/* eslint-disable no-console */
-/* eslint-disable react/jsx-no-bind */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useGetModelsQuery } from "@/api/integrations";
 import { PROMPT_PAYLOAD_KEY, SOURCE_PROJECT_ID } from "@/common/constants.js";
+import { StyledGridContainer, LeftGridItem, RrightGridItem, StyledInputEnhancer, StyledAvatar, TabBarItems, SelectLabel } from "./Common"
+import FileReaderEnhancer from "./FileReaderInput"
+import VariableList from "./VariableList"
 import BasicAccordion from "@/components/BasicAccordion";
 import Button from "@/components/Button";
 import ChatBox from "@/components/ChatBox/ChatBox";
 import SettingIcon from "@/components/Icons/SettingIcon";
 import SingleSelect from "@/components/SingleSelect";
 import Slider from "@/components/Slider";
-import { actions as promptSliceActions } from "@/reducers/prompts";
-import { Avatar, Grid, TextField, Typography } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getFileFormat, contextResolver } from "@/common/utils";
-import YAML from "js-yaml";
 import Messages from "./Messages";
 
-const StyledGridContainer = styled(Grid)(() => ({
-  padding: 0,
-}));
+// const LeftGridItem = styled(Grid)(() => ({
+//   position: "relative",
+//   padding: "0 0.75rem",
+// }));
 
-const LeftGridItem = styled(Grid)(() => ({
-  position: "relative",
-  padding: "0 0.75rem",
-}));
+// const RrightGridItem = styled(Grid)(() => ({
+//   padding: "0 0.75rem",
+// }));
 
-const RrightGridItem = styled(Grid)(() => ({
-  padding: "0 0.75rem",
-}));
+// const StyledInput = styled(TextField)(() => ({
+//   marginBottom: "0.75rem",
+//   "& .MuiFormLabel-root": {
+//     fontSize: "0.875rem",
+//     lineHeight: "1.375rem",
+//     top: "-0.25rem",
+//     left: "0.75rem",
+//   },
+//   "& .MuiInputBase-root": {
+//     padding: "1rem 0.75rem",
+//     marginTop: "0",
+//   },
+// }));
 
-const StyledInput = styled(TextField)(() => ({
-  marginBottom: "0.75rem",
-  "& .MuiFormLabel-root": {
-    fontSize: "0.875rem",
-    lineHeight: "1.375rem",
-    top: "-0.25rem",
-    left: "0.75rem",
-  },
-  "& .MuiInputBase-root": {
-    padding: "1rem 0.75rem",
-    marginTop: "0",
-  },
-}));
+// const FileReaderEnhancer = (props) => {
+//   const dispatch = useDispatch();
+//   const [inputValue, setInputValue] = useState("");
+//   const [highlightContext, setHighlightContext] = useState(false);
 
-const StyledInputEnhancer = (props) => {
-  const dispatch = useDispatch();
-  const { payloadkey, label } = props;
-  const { currentPrompt } = useSelector((state) => state.prompts);
-  let theValue = currentPrompt && currentPrompt[payloadkey];
-  if (payloadkey === PROMPT_PAYLOAD_KEY.variables) {
-    theValue = theValue.find((item) => item.key === label).value;
-  }
-  const [value, setValue] = useState(
-    payloadkey === PROMPT_PAYLOAD_KEY.tags
-      ? theValue?.map((tag) => tag?.tag).join(",")
-      : theValue
-  );
-  const handlers = {
-    onBlur: useCallback((event) => {
-      if (payloadkey === PROMPT_PAYLOAD_KEY.variables) return;
-      const { target } = event;
-      const inputValue = target?.value;
-      if (payloadkey === PROMPT_PAYLOAD_KEY.context) {
-        dispatch(
-          promptSliceActions.updateCurrentPromptData({
-            key: PROMPT_PAYLOAD_KEY.variables,
-            data: contextResolver(inputValue).map((variable) => {
-              return {
-                key: variable,
-                value: "",
-              };
-            }),
-          })
-        );
-      } else {
-        dispatch(
-          promptSliceActions.updateCurrentPromptData({
-            key: payloadkey,
-            data:
-              payloadkey === PROMPT_PAYLOAD_KEY.tags
-                ? target?.value?.split(",")
-                : target?.value,
-          })
-        );
-      }
-    }, []),
-    onChange: useCallback((event) => {
-      const { target } = event;
-      setValue(target?.value);
-    }),
-  };
-  return <StyledInput value={value} {...handlers} {...props} />;
-};
+//   const updateVariables = (context) => {
+//     dispatch(
+//       promptSliceActions.updateCurrentPromptData({
+//         key: PROMPT_PAYLOAD_KEY.variables,
+//         data: contextResolver(context).map((variable) => {
+//           return {
+//             key: variable,
+//             value: "",
+//           };
+//         }),
+//       })
+//     );
+//   };
 
-const FileReaderEnhancer = (props) => {
-  const dispatch = useDispatch();
-  const [inputValue, setInputValue] = useState("");
-  const [highlightContext, setHighlightContext] = useState(false);
+//   const handleInput = useCallback((event) => {
+//     event.preventDefault();
+//     setInputValue(event.target.value);
+//   });
 
-  const updateVariables = (context) => {
-    dispatch(
-      promptSliceActions.updateCurrentPromptData({
-        key: PROMPT_PAYLOAD_KEY.variables,
-        data: contextResolver(context).map((variable) => {
-          return {
-            key: variable,
-            value: "",
-          };
-        }),
-      })
-    );
-  };
+//   const handleDragOver = useCallback(() => {
+//     (event) => event.preventDefault();
+//     if (highlightContext) return;
+//     setHighlightContext(true);
+//   });
 
-  const handleInput = useCallback((event) => {
-    event.preventDefault();
-    setInputValue(event.target.value);
-  });
+//   const handleDragLeave = useCallback(() => {
+//     (event) => event.preventDefault();
+//     setHighlightContext(false);
+//   });
 
-  const handleDragOver = useCallback(() => {
-    (event) => event.preventDefault();
-    if (highlightContext) return;
-    setHighlightContext(true);
-  });
+//   const handleDrop = useCallback((event) => {
+//     event.preventDefault();
+//     setHighlightContext(false);
 
-  const handleDragLeave = useCallback(() => {
-    (event) => event.preventDefault();
-    setHighlightContext(false);
-  });
+//     const file = event.dataTransfer.files[0];
+//     const fileName = file.name;
+//     const reader = new FileReader();
+//     reader.onload = () => {
+//       try {
+//         let fileData = null;
+//         const fileFormat = getFileFormat(fileName);
+//         const dataString = reader.result;
+//         if (fileFormat === "yaml") {
+//           const yamlData = YAML.load(dataString);
+//           fileData = yamlData;
+//         } else {
+//           const jsonData = JSON.parse(dataString);
+//           fileData = jsonData;
+//         }
+//         const { context } = fileData;
+//         updateVariables(context || "");
+//         setInputValue(context);
+//       } catch (error) {
+//         console.error("Error parsing File:", error);
+//       }
+//     };
 
-  const handleDrop = useCallback((event) => {
-    event.preventDefault();
-    setHighlightContext(false);
+//     reader.readAsText(file);
+//   });
 
-    const file = event.dataTransfer.files[0];
-    const fileName = file.name;
-    const reader = new FileReader();
-    reader.onload = () => {
-      try {
-        let fileData = null;
-        const fileFormat = getFileFormat(fileName);
-        const dataString = reader.result;
-        if (fileFormat === "yaml") {
-          const yamlData = YAML.load(dataString);
-          fileData = yamlData;
-        } else {
-          const jsonData = JSON.parse(dataString);
-          fileData = jsonData;
-        }
-        const { context } = fileData;
-        updateVariables(context || "");
-        setInputValue(context);
-      } catch (error) {
-        console.error("Error parsing File:", error);
-      }
-    };
+//   return (
+//     <StyledInputEnhancer
+//       value={inputValue}
+//       style={{ backgroundColor: highlightContext ? "#3d3d3d" : "" }}
+//       onDrop={handleDrop}
+//       onDragOver={handleDragOver}
+//       onDragLeave={handleDragLeave}
+//       onInput={handleInput}
+//       {...props}
+//     />
+//   );
+// };
 
-    reader.readAsText(file);
-  });
+// const VariableList = (props) => {
+//   const dispatch = useDispatch();
+//   const { currentPrompt } = useSelector((state) => state.prompts);
+//   const variables = [].concat(currentPrompt[PROMPT_PAYLOAD_KEY.variables]);
 
-  return (
-    <StyledInputEnhancer
-      value={inputValue}
-      style={{ backgroundColor: highlightContext ? "#3d3d3d" : "" }}
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
-      onDragLeave={handleDragLeave}
-      onInput={handleInput}
-      {...props}
-    />
-  );
-};
+//   const handleInput = useCallback((event, updateKey) => {
+//     event.preventDefault();
+//     const value = event.target.value;
+//     dispatch(
+//       promptSliceActions.updateSpecificVariable({
+//         key: PROMPT_PAYLOAD_KEY.variables,
+//         updateKey,
+//         data: value,
+//       })
+//     );
+//     return;
+//   });
 
-const VariableList = (props) => {
-  const dispatch = useDispatch();
-  const { currentPrompt } = useSelector((state) => state.prompts);
-  const variables = [].concat(currentPrompt[PROMPT_PAYLOAD_KEY.variables]);
+//   return (
+//     <div>
+//       {variables.map(({ key }) => {
+//         return (
+//           <StyledInputEnhancer
+//             key={key}
+//             label={key}
+//             onInput={(event) => handleInput(event, key)}
+//             {...props}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
-  const handleInput = useCallback((event, updateKey) => {
-    event.preventDefault();
-    const value = event.target.value;
-    dispatch(
-      promptSliceActions.updateSpecificVariable({
-        key: PROMPT_PAYLOAD_KEY.variables,
-        updateKey,
-        data: value,
-      })
-    );
-    return;
-  });
+// const StyledAvatar = styled(Avatar)(({ theme }) => ({
+//   width: "1.75rem",
+//   height: "1.75rem",
+//   display: "flex",
+//   flex: "0 0 1.75rem",
+//   alignItems: "center",
+//   justifyContent: "center",
+//   backgroundColor: theme.palette.secondary.main,
+// }));
 
-  return (
-    <div>
-      {variables.map(({ key }) => {
-        return (
-          <StyledInputEnhancer
-            key={key}
-            label={key}
-            onInput={(event) => handleInput(event, key)}
-            {...props}
-          />
-        );
-      })}
-    </div>
-  );
-};
+// const TabBarItems = styled("div")(() => ({
+//   position: "absolute",
+//   top: "-3.7rem",
+//   right: "0.5rem",
+// }));
 
-const StyledAvatar = styled(Avatar)(({ theme }) => ({
-  width: "1.75rem",
-  height: "1.75rem",
-  display: "flex",
-  flex: "0 0 1.75rem",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundColor: theme.palette.secondary.main,
-}));
-
-const TabBarItems = styled("div")(() => ({
-  position: "absolute",
-  top: "-3.7rem",
-  right: "0.5rem",
-}));
-
-const SelectLabel = styled(Typography)(() => ({
-  display: "inline-block",
-}));
+// const SelectLabel = styled(Typography)(() => ({
+//   display: "inline-block",
+// }));
 
 const promptDetailLeft = [
   {
