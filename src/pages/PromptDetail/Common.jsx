684fbc4602579f0b26bc750cbcf591fa9a798693
Hawk.Qing <hawk_qing@epam.com>
2023-11-14 11:12:07 +0800

    fix: slider stepper issue

--- src/pages/PromptDetail/Common.jsx
+++ src/pages/PromptDetail/Common.jsx
@@ -36,6 +36,17 @@ export const StyledInput = styled(TextField)(() => ({
     padding: "1rem 0.75rem",
     marginTop: "0",
   },
+  '& input[type=number]': {
+    'MozAppearance': 'textfield'
+  },
+  '& input[type=number]::-webkit-outer-spin-button': {
+    'WebkitAppearance': 'none',
+    margin: 0
+  },
+  '& input[type=number]::-webkit-inner-spin-button': {
+    'WebkitAppearance': 'none',
+    margin: 0
+  }
 }));
 
 export const StyledAvatar = styled(Avatar)(({ theme }) => ({
@@ -59,57 +70,57 @@ export const SelectLabel = styled(Typography)(() => ({
 }));
 
 export const StyledInputEnhancer = (props) => {
-    const dispatch = useDispatch();
-    const { payloadkey, label } = props;
-    const { currentPrompt } = useSelector((state) => state.prompts);
-    let theValue = currentPrompt && currentPrompt[payloadkey];
-    if (payloadkey === PROMPT_PAYLOAD_KEY.variables) {
-      theValue = theValue.find((item) => item.key === label).value;
-    }
-    const [value, setValue] = useState(
-      payloadkey === PROMPT_PAYLOAD_KEY.tags
-        ? theValue?.map((tag) => tag?.tag).join(",")
-        : theValue
-    );
-    const handlers = {
-      onBlur: useCallback((event) => {
-        if (payloadkey === PROMPT_PAYLOAD_KEY.variables) return;
-        const { target } = event;
-        const inputValue = target?.value;
-        if (payloadkey === PROMPT_PAYLOAD_KEY.context) {
-          dispatch(
-            promptSliceActions.updateCurrentPromptData({
-              key: PROMPT_PAYLOAD_KEY.variables,
-              data: contextResolver(inputValue).map((variable) => {
-                return {
-                  key: variable,
-                  value: "",
-                };
-              }),
-            })
-          );
-        } else {
-          dispatch(
-            promptSliceActions.updateCurrentPromptData({
-              key: payloadkey,
-              data:
-                payloadkey === PROMPT_PAYLOAD_KEY.tags
-                  ? target?.value?.split(",")
-                  : target?.value,
-            })
-          );
-        }
-      }, [dispatch, payloadkey]),
-      onChange: useCallback((event) => {
-        const { target } = event;
-        setValue(target?.value);
-      }, []),
-    };
-    return <StyledInput 
-      variant="standard"
-      fullWidth
-      value={value} 
-      {...handlers} 
-      {...props} 
-    />;
+  const dispatch = useDispatch();
+  const { payloadkey, label } = props;
+  const { currentPrompt } = useSelector((state) => state.prompts);
+  let theValue = currentPrompt && currentPrompt[payloadkey];
+  if (payloadkey === PROMPT_PAYLOAD_KEY.variables) {
+    theValue = theValue.find((item) => item.key === label).value;
+  }
+  const [value, setValue] = useState(
+    payloadkey === PROMPT_PAYLOAD_KEY.tags
+      ? theValue?.map((tag) => tag?.tag).join(",")
+      : theValue
+  );
+  const handlers = {
+    onBlur: useCallback((event) => {
+      if (payloadkey === PROMPT_PAYLOAD_KEY.variables) return;
+      const { target } = event;
+      const inputValue = target?.value;
+      if (payloadkey === PROMPT_PAYLOAD_KEY.context) {
+        dispatch(
+          promptSliceActions.updateCurrentPromptData({
+            key: PROMPT_PAYLOAD_KEY.variables,
+            data: contextResolver(inputValue).map((variable) => {
+              return {
+                key: variable,
+                value: "",
+              };
+            }),
+          })
+        );
+      } else {
+        dispatch(
+          promptSliceActions.updateCurrentPromptData({
+            key: payloadkey,
+            data:
+              payloadkey === PROMPT_PAYLOAD_KEY.tags
+                ? target?.value?.split(",")
+                : target?.value,
+          })
+        );
+      }
+    }, [dispatch, payloadkey]),
+    onChange: useCallback((event) => {
+      const { target } = event;
+      setValue(target?.value);
+    }, []),
   };
+  return <StyledInput
+    variant="standard"
+    fullWidth
+    value={value}
+    {...handlers}
+    {...props}
+  />;
+};
