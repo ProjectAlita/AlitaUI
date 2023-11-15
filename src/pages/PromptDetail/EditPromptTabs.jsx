08c7f457781d2a784ea80df70548f4db923360cd
Hawk.Qing <hawk_qing@epam.com>
2023-11-15 15:45:56 +0800

    fix: extra title bar

--- src/pages/PromptDetail/EditPromptTabs.jsx
+++ src/pages/PromptDetail/EditPromptTabs.jsx
@@ -1,29 +1,16 @@
-import StyledLabel from '@/components/StyledLabel';
 import StyledTabs from '@/components/StyledTabs';
 import styled from '@emotion/styled';
 import { Grid } from '@mui/material';
 import * as React from 'react';
-import { useSelector } from 'react-redux';
 
-const Label = styled(StyledLabel)(({theme}) => ({
-  marginBottom: theme.spacing(1.5)
-}));
 
 const TabContentDiv = styled('div')(({theme}) => ({
   padding: `${theme.spacing(3)} ${theme.spacing(0.5)}`,
 }))
 
 export default function EditPromptTabs({runTabContent}) {
-  const { currentPrompt } = useSelector((state) => state.prompts);
-  const title = React.useMemo(() => {
-    return currentPrompt?.id ? currentPrompt?.name : 'Create Prompt';
-  }, [currentPrompt]);
-
   return <React.Fragment>
   <Grid container sx={{padding: '0.5rem 1.5rem'}}>
-    <Grid item xs={12}>
-      <Label>{title}</Label>
-    </Grid>
     <Grid item xs={12}>
       <StyledTabs tabs={[{
         label: 'Run',
