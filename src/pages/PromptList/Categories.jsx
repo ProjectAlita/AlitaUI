ec59555b352543dae2fa20db285c78ccb6f1e087
Kevin_Cai <Kevin_Cai@epam.com>
2023-11-23 16:28:54 +0800

    fix: 1.more space between header & tabs; 2.add clear function temporarily

--- src/pages/PromptList/Categories.jsx
+++ src/pages/PromptList/Categories.jsx
@@ -1,13 +1,13 @@
 import { useTagListQuery } from '@/api/prompts';
 import { SOURCE_PROJECT_ID } from '@/common/constants';
 import { renderStatusComponent } from '@/common/utils';
-import StyledLabel from "@/components/StyledLabel";
+import StyledLabel from '@/components/StyledLabel';
 import { Chip, Typography } from '@mui/material';
 import { useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Label = styled(StyledLabel)(({ theme }) => ({
-  marginBottom: theme.spacing(3)
+  marginBottom: theme.spacing(3),
 }));
 
 const StyledChip = styled(Chip)(() => ({
@@ -25,55 +25,89 @@ const StyledChip = styled(Chip)(() => ({
     fontWeight: '500',
     lineHeight: '1rem',
     opacity: '0.8',
-  }
+  },
 }));
 
 const Categories = ({ tagList, selectedTags }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const { isSuccess, isError, isLoading } = useTagListQuery(SOURCE_PROJECT_ID);
-  const handleClick = useCallback((newTagId) => {
-    if (isNaN(newTagId)) return;
+  const handleClick = useCallback(
+    (newTagId) => {
+      if (isNaN(newTagId)) return;
 
-    const isExistingTag = selectedTags.includes(newTagId);
-    const tags = isExistingTag ?
-      selectedTags.filter(tag => tag !== newTagId) :
-      [...selectedTags, newTagId];
+      const isExistingTag = selectedTags.includes(newTagId);
+      const tags = isExistingTag
+        ? selectedTags.filter((tag) => tag !== newTagId)
+        : [...selectedTags, newTagId];
 
-    const currentQueryParam = location.search ? new URLSearchParams(location.search) : new URLSearchParams();
-    if (tags.length > 0) {
-      currentQueryParam.set("tags", tags.join(","));
-    } else {
-      currentQueryParam.delete("tags");
-    }
+      const currentQueryParam = location.search
+        ? new URLSearchParams(location.search)
+        : new URLSearchParams();
+      if (tags.length > 0) {
+        currentQueryParam.set('tags', tags.join(','));
+      } else {
+        currentQueryParam.delete('tags');
+      }
 
-    navigate({
-      pathname: location.pathname,
-      search: currentQueryParam.toString()
-    }, { replace: true });
-  }, [location.pathname, location.search, navigate, selectedTags]);
+      navigate(
+        {
+          pathname: location.pathname,
+          search: currentQueryParam.toString(),
+        },
+        { replace: true }
+      );
+    },
+    [location.pathname, location.search, navigate, selectedTags]
+  );
 
-  const successContent = (
-    tagList.length > 0 ?
+  const handleClear = useCallback(() => {
+    navigate(
+      {
+        pathname: location.pathname
+      },
+      { replace: true }
+    );
+  }, [location.pathname, navigate])
+
+  const successContent =
+    tagList.length > 0 ? (
       tagList.map(({ id, name }) => (
-        <StyledChip key={id}
+        <StyledChip
+          key={id}
           label={name}
           // eslint-disable-next-line react/jsx-no-bind
           onClick={() => handleClick(id)}
           variant={selectedTags.includes(id) ? 'outlined' : 'filled'}
         />
-      )) :
+      ))
+    ) : (
       <Typography variant={'body2'}>None.</Typography>
-  );
+    );
 
   return (
     <div style={{ maxHeight: '392px', marginBottom: '16px' }}>
-      <div>
-        <Label>Categories</Label>
+      <div style={{ display: 'flex' }}>
+        <div>
+          <Label>Categories</Label>
+        </div>
+        <div style={{ marginLeft: '2rem' }}>
+          <Label
+            style={{
+              border: '1px solid',
+              borderRadius: '8px',
+              padding: '0 0.5rem 0 0.5rem',
+              caretColor: 'transparent',
+            }}
+            onClick={handleClear}
+          >
+            Clear
+          </Label>
+        </div>
       </div>
       {renderStatusComponent({ isLoading, isSuccess, isError, successContent })}
     </div>
   );
-}
+};
 
 export default Categories;
