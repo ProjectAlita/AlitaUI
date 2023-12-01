f3244c61298a601ee533bf3b60e17f9bc7d9e518
Kevin_Cai <Kevin_Cai@epam.com>
2023-12-01 10:13:41 +0800

    refactor: extract card popover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,16 @@
 import { ContentType, PromptStatus, ViewMode } from '@/common/constants';
 import { getInitials, stringToColor } from '@/common/utils';
 import isPropValid from '@emotion/is-prop-valid';
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'; 
 import { Avatar } from '@mui/material';
-import Popover from '@mui/material/Popover';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import {
   useCallback,
   useEffect,
   useRef,
   useState,
-  forwardRef,
-  useImperativeHandle,
-  isValidElement,
 } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import CommentIcon from './Icons/CommentIcon';
@@ -27,6 +20,7 @@ import StarIcon from './Icons/StarIcon';
 import StarActiveIcon from './Icons/StarActiveIcon';
 import TrophyIcon from './Icons/TrophyIcon';
 import BookmarkIcon from './Icons/BookmarkIcon';
+import CardPopover from '@/components/CardPopover'
 
 const MOCK_ISTOP = true;
 const MOCK_FAVORITE_COUNT = 20;
@@ -424,105 +418,6 @@ const InfoContainer = ({type = ContentType.Prompts, id, name}) => {
     </>
   );
 };
-
-const StyledPopoverContainer = styled(Popover)(({ theme }) => ({
-  borderRadius: '0.5rem',
-  '& > div': {
-    border: `1px solid ${theme.palette.border.lines}`,
-  },
-  '& ul': {
-    padding: '0',
-  },
-}));
-
-const StyledPopoverItem = styled(ListItem)(() => ({
-  padding: '.5rem 1rem',
-}));
-
-const StyledAuthorPopoverItem = styled('div')(() => ({
-  cursor: 'pointer',
-  caretColor: 'transparent',
-  display: 'flex',
-  padding: '0',
-  '&:hover': {
-    color: 'white'
-  }
-}));
-
-const CardPopover = forwardRef((props, ref) => {
-  const { contentList, type } = props;
-  const [anchorEl, setAnchorEl] = useState(null);
-  const avatarStyle = {
-    marginRight: '.5rem',
-    padding: '0',
-    width: '20px',
-    height: '20px',
-  };
-
-  useImperativeHandle(ref, () => ({
-    handleClick,
-  }));
-
-  const handleClick = useCallback((event) => {
-    setAnchorEl(event.currentTarget);
-  }, []);
-
-  const handleClose = useCallback(() => {
-    setAnchorEl(null);
-  }, []);
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'card-popover' : undefined;
-
-  return (
-    <StyledPopoverContainer
-      id={id}
-      open={open}
-      anchorEl={anchorEl}
-      onClose={handleClose}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'left',
-      }}
-    >
-      <List>
-        {contentList.map((content, index) => {
-          const contentMap = {
-            author: (
-              <StyledAuthorPopoverItem>
-                <Avatar
-                  key={`${content.id || content.name || content}-${index}`}
-                  style={{
-                    ...avatarStyle,
-                  }}
-                  {...stringAvatar(content.name)}
-                />
-                <div>{content.name}</div>
-              </StyledAuthorPopoverItem>
-            ),
-            category: content.name,
-          };
-          return (
-            <StyledPopoverItem
-              key={`${content.id || content.name || content}-${index}`}
-            >
-              <ListItemText
-                component={'div'}
-                primary={
-                  <Typography component='div' variant='body2'>
-                    {isValidElement(content) ? content : contentMap[type]}
-                  </Typography>
-                }
-              />
-            </StyledPopoverItem>
-          );
-        })}
-      </List>
-    </StyledPopoverContainer>
-  );
-});
-
-CardPopover.displayName = 'CardPopover';
 
 export default function PromptCard({ data = {}, viewMode, type }) {
   const { id, name = '', description = '', authors = [], tags = [], promptCount = 0 } = data;
