760af511d2b991822eb7cbddb401cccad40a7f28
Hawk.Qing <hawk_qing@epam.com>
2023-12-07 18:16:03 +0800

    refactor: make getStatusColor reusable

--- src/components/StatusDot.jsx
+++ src/components/StatusDot.jsx
@@ -1,27 +1,9 @@
 import  {useTheme } from '@mui/material/styles';
+import { getStatusColor } from '@/common/utils';
+
 import CircleIcon from './Icons/CircleIcon';
 
 export function StatusDot ({status, size = 'auto'}) {
   const theme = useTheme();
-  let color = '';
-  switch (status) {
-    case 'draft':
-      color = theme.palette.status.draft;
-      break;
-    case 'on_moderation':
-      color = theme.palette.status.onModeration;
-      break;
-    case 'published':
-      color = theme.palette.status.published;
-      break;
-    case 'rejected':
-      color = theme.palette.status.rejected;
-      break;
-    case 'user_approval':
-      color = theme.palette.status.userApproval;
-      break;
-    default:
-      break;
-  }
-  return <CircleIcon fill={color} size={size}/>;
+  return <CircleIcon fill={getStatusColor(status, theme)} size={size}/>;
 }
