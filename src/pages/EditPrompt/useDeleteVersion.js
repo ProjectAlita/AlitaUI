3d025113e84f716a7b866225784558f0b872d343
Hawk.Qing <hawk_qing@epam.com>
2023-12-15 16:15:17 +0800

    fix: wrong navigation after delete a version

--- src/pages/EditPrompt/useDeleteVersion.js
+++ src/pages/EditPrompt/useDeleteVersion.js
@@ -1,10 +1,23 @@
 import { useDeleteVersionMutation } from '@/api/prompts';
 import { useEffect, useCallback } from 'react';
 import { buildErrorMessage } from '@/common/utils';
-import { useProjectId } from '../hooks';
+import { useProjectId, useFromMyLibrary, useCollectionFromUrl, useNameFromUrl, useViewModeFromUrl } from '../hooks';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import RouteDefinitions from '@/routes';
+import { SearchParams } from '@/common/constants';
 
 const useDeleteVersion = (currentVersionId, promptId, setOpenToast, setToastSeverity, setToastMessage) => {
+  const navigate = useNavigate();
   const projectId = useProjectId();
+  const { versions } = useSelector((state) => state.prompts);
+  const isFromMyLibrary = useFromMyLibrary();
+  const collection = useCollectionFromUrl();
+  const { tab, collectionId } = useParams();
+  const promptName = useNameFromUrl();
+  const { state } = useLocation();
+  const viewMode = useViewModeFromUrl();
+
   const [deleteVersion, {
     isLoading: isDeletingVersion,
     isSuccess: isDeleteVersionSuccess,
@@ -38,12 +51,75 @@ const useDeleteVersion = (currentVersionId, promptId, setOpenToast, setToastSeve
     [currentVersionId, deleteVersion, projectId, promptId],
   );
 
+  const handleDeleteSuccess = useCallback(
+    () => {
+      const newVersion = versions.find(item => item.name === 'latest') || versions.find(item => item.id !== currentVersionId);
+      const newVersionName = newVersion?.name;
+      if (newVersion) {
+        navigate(
+          isFromMyLibrary ?
+            collectionId ?
+              `${RouteDefinitions.MyLibrary}/collections/${collectionId}/prompts/${promptId}/${encodeURIComponent(newVersionName)}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}&${SearchParams.Collection}=${collection}`
+              :
+              `${RouteDefinitions.MyLibrary}/prompts/${promptId}/${encodeURIComponent(newVersionName)}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}`
+            :
+            tab ?
+              `${RouteDefinitions.Prompts}/${tab}/${promptId}/${encodeURIComponent(newVersionName)}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}`
+              :
+              `${RouteDefinitions.Prompts}/${promptId}/${encodeURIComponent(newVersionName)}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}`,
+          {
+            state,
+            replace: true,
+          });
+      } else {
+        navigate(
+          isFromMyLibrary ?
+            collectionId ?
+              `${RouteDefinitions.MyLibrary}/collections/${collectionId}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}&${SearchParams.Collection}=${collection}`
+              :
+              `${RouteDefinitions.MyLibrary}/prompts?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}`
+            :
+            tab ?
+              `${RouteDefinitions.Prompts}/${tab}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}`
+              :
+              `${RouteDefinitions.Prompts}?${SearchParams.ViewMode}=${viewMode}&${SearchParams.Name}=${promptName}`,
+          {
+            state,
+            replace: true,
+          });
+      }
+      resetDeleteVersion();
+    },
+    [
+      collection,
+      collectionId,
+      currentVersionId,
+      isFromMyLibrary,
+      navigate,
+      promptId,
+      promptName,
+      resetDeleteVersion,
+      state,
+      tab,
+      versions,
+      viewMode
+    ]
+  );
+
+  useEffect(
+    () => {
+      if (isDeleteVersionError) {
+        resetDeleteVersion();
+      } else if (isDeleteVersionSuccess) {
+        handleDeleteSuccess();
+      }
+    },
+    [handleDeleteSuccess, isDeleteVersionError, isDeleteVersionSuccess, resetDeleteVersion]);
+
   return {
     doDeleteVersion,
     isDeletingVersion,
-    isDeleteVersionSuccess,
-    isDeleteVersionError,
-    resetDeleteVersion
+    resetDeleteVersion,
   };
 }
 
