151b2378c99b28ecd92f447555e8acf1507f855f
Jackson Wang <wzx91188@gmail.com>
2024-01-09 14:56:30 +0800

    feat/handle-unauthorized-response (#246)

--- src/api/alitaApi.js
+++ src/api/alitaApi.js
@@ -4,24 +4,34 @@ import { DEV, VITE_DEV_TOKEN, VITE_SERVER_URL } from "../common/constants.js";
 
 // https://redux-toolkit.js.org/rtk-query/api/createApi
 export const alitaApi = createApi({
-    reducerPath: 'alitaApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: VITE_SERVER_URL,
-        // mode: "cors",
-        prepareHeaders: (headers) => {
-            if (DEV) {
-                VITE_DEV_TOKEN && headers.set('Authorization', `Bearer ${VITE_DEV_TOKEN}`)
-                headers.set('Cache-Control', 'no-cache')
-            }
-            return headers
-        },
-    }),
-    tagTypes: [], // Here we specify tags for caching and invalidation
-    endpoints: () => ({})
+  reducerPath: 'alitaApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: VITE_SERVER_URL,
+    // mode: "cors",
+    fetchFn: async (request) => {
+      const response = await fetch(request);
+      if (response.redirected) {
+        const redirectUrl = response.url;
+        window.location.href = redirectUrl
+        return { redirectUrl };
+      }
+
+      return response;
+    },
+    prepareHeaders: (headers) => {
+      if (DEV) {
+        VITE_DEV_TOKEN && headers.set('Authorization', `Bearer ${VITE_DEV_TOKEN}`)
+        headers.set('Cache-Control', 'no-cache')
+      }
+      return headers
+    },
+  }),
+  tagTypes: [], // Here we specify tags for caching and invalidation
+  endpoints: () => ({})
 })
 
 export const {
-    middleware,
-    reducer,
-    reducerPath,
+  middleware,
+  reducer,
+  reducerPath,
 } = alitaApi
