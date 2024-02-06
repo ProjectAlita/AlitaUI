829922817819f8530c80e8387b7d1915f44108be
Jackson Wang <wzx91188@gmail.com>
2024-02-06 18:14:45 +0800

    fix/switch-project-integration-and-issue (#358)

--- src/pages/MyLibrary/RightPanel.jsx
+++ src/pages/MyLibrary/RightPanel.jsx
@@ -1,7 +1,8 @@
 import { ViewMode } from '@/common/constants';
 import AuthorInformation from '@/components/AuthorInformation';
 import Categories from '@/components/Categories';
-import { useViewMode } from '@/pages/hooks';
+import { useSelectedProjectId, useViewMode } from '@/pages/hooks';
+import { useSelector } from 'react-redux';
 import { rightPanelStyle, tagsStyle } from './CommonStyles';
 import ProjectSelect from './ProjectSelect';
 import useQueryTrendingAuthor from './useQueryTrendingAuthor';
@@ -9,13 +10,19 @@ import useQueryTrendingAuthor from './useQueryTrendingAuthor';
 export default function RightPanel({ tagList }) {
   const { isLoadingAuthor } = useQueryTrendingAuthor();
   const viewMode = useViewMode();
+  const { personal_project_id: privateProjectId } = useSelector(state => state.user);
+  const selectedProjectId = useSelectedProjectId();
   return (
     <div style={rightPanelStyle}>
       {
         viewMode === ViewMode.Owner && <ProjectSelect />
       }
       <Categories tagList={tagList} title='Tags' style={tagsStyle} />
-      <AuthorInformation isLoading={isLoadingAuthor} />
+      {
+        selectedProjectId === privateProjectId ?
+          <AuthorInformation isLoading={isLoadingAuthor} /> :
+          null
+      }
     </div>
   )
 }
