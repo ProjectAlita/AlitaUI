5e23dde0f3442ffd341fa7285519ad78653e4862
Jackson Wang <Jackson_Wang@epam.com>
2024-02-20 11:45:37 +0800

    feat/dataset-view-mode

--- src/pages/DataSources/Components/Sources/SourceGit.jsx
+++ src/pages/DataSources/Components/Sources/SourceGit.jsx
@@ -1,132 +1,107 @@
-/* eslint-disable */
-import React, {useCallback, useEffect} from "react";
-import {Box, ToggleButton, ToggleButtonGroup} from "@mui/material";
+/* eslint-disable react/jsx-no-bind */
 import BasicAccordion from "@/components/BasicAccordion.jsx";
 import CheckLabel from "@/components/CheckLabel.jsx";
 import SingleSelect from "@/components/SingleSelect.jsx";
-import {StyledInput} from "@/pages/EditPrompt/Common.jsx";
-
-
-const documentLoaders = {
-  TextLoader: {
-    label: 'TextLoader',
-    value: 'text_loader'
-  },
-}
+import { gitTypes, documentLoaders } from "@/pages/DataSources/constants";
+import { StyledInput } from "@/pages/EditPrompt/Common.jsx";
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { useCallback, useMemo } from "react";
 
 const documentLoadersOptions = Object.values(documentLoaders)
 
-const gitTypes = {
-  ssh: {
-    label: 'SSH',
-    value: 'ssh'
-  },
-  https: {
-    label: 'HTTPS',
-    value: 'https'
-  },
-}
 
 const gitTypeOptions = Object.values(gitTypes)
 
-const SourceGit = ({formik}) => {
-  const {url, branch, type, sshKey, username, password, advanced} = formik.values.source?.options
-  const {multithreading, defaultLoader, extWhitelist, extBlacklist} = advanced || {}
-  const errors = formik.errors.source
+const SourceGit = ({ formik, readOnly }) => {
+  const { url, branch, type, sshKey, username, password, advanced } = formik?.values?.source?.options || {}
+  const { multithreading, defaultLoader, extWhitelist, extBlacklist } = advanced || {}
   const handleChange = useCallback((field, value) => {
     formik.setFieldValue('source.options.' + field, value)
   }, [formik])
-  useEffect(() => {
-    handleChange('url', '')
-    handleChange('branch', 'main')
-    handleChange('type', gitTypes.ssh.value)
-    handleChange('username', '')
-    handleChange('password', '')
-    handleChange('sshKey', '')
-    
-    handleChange('advanced.defaultLoader', documentLoaders.TextLoader.value)
-    handleChange('advanced.multithreading', false)
-    handleChange('advanced.extWhitelist', '')
-    handleChange('advanced.extBlacklist', '')
-  }, [])
+
+
+  const inputProps = useMemo(() => ({
+    disabled: readOnly,
+    fullWidth: true,
+    variant: 'standard',
+    onChange: formik.handleChange,
+    onBlur: formik.handleBlur
+  }), [formik.handleBlur, formik.handleChange, readOnly])
+
+  const handleToggle = useCallback(e => {
+    handleChange('type', e.target.value);
+  }, [handleChange]);
+
   return (
     <>
       <Box display={"flex"} width={'100%'}>
         <StyledInput
           name='source.options.url'
-          variant='standard'
-          fullWidth
           label='URL'
           value={url}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          sx={{flexGrow: 1}}
+          sx={{ flexGrow: 1 }}
+          {...inputProps}
         />
         <Box alignSelf={'end'}>
           <ToggleButtonGroup
             size="small"
             value={type}
-            onChange={e => handleChange('type', e.target.value)}
+            onChange={readOnly ? undefined : handleToggle}
           >
             {gitTypeOptions.map(i => (
-              <ToggleButton value={i.value} key={i.value} >{i.label}</ToggleButton>
+              <ToggleButton
+                key={i.value}
+                value={i.value}
+                disabled={readOnly}
+              >
+                {i.label}
+              </ToggleButton>
             ))}
           </ToggleButtonGroup>
         </Box>
       </Box>
       <StyledInput
         name='source.options.branch'
-        variant='standard'
-        fullWidth
         label='Branch'
         value={branch}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
+        {...inputProps}
       />
       {type === gitTypes.ssh.value &&
         <StyledInput
           name='source.options.sshKey'
-          variant='standard'
-          fullWidth
           label='SSH Key'
           value={sshKey}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          {...inputProps}
         />
       }
       {type === gitTypes.https.value &&
         <>
           <StyledInput
             name='source.options.username'
-            variant='standard'
-            fullWidth
             label='Username'
             value={username}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            {...inputProps}
           />
           <StyledInput
             name='source.options.password'
             type={'password'}
             autoComplete={'off'}
-            variant='standard'
-            fullWidth
             label='Password'
             value={password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            {...inputProps}
           />
         </>
       }
       <BasicAccordion
         uppercase={false}
-        style={{width: '100%'}}
+        style={{ width: '100%' }}
         items={[
           {
             title: 'Advanced settings',
             content: (
               <Box pl={3} width={'100%'}>
                 <CheckLabel
+                  disabled={readOnly}
                   label='Multithreading'
                   checked={multithreading || false}
                   onChange={e => handleChange('advanced.multithreading', e.target.checked)}
@@ -138,30 +113,25 @@ const SourceGit = ({formik}) => {
                   value={defaultLoader}
                   options={documentLoadersOptions}
                   customSelectedFontSize={'0.875rem'}
-                  sx={{marginTop: '8px'}}
+                  sx={{ marginTop: '8px' }}
+                  disabled={readOnly}
                 />
                 <StyledInput
                   name='source.options.advanced.extWhitelist'
-                  variant='standard'
-                  fullWidth
                   label='Extension whitelist'
                   value={extWhitelist}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
+                  {...inputProps}
                 />
                 <StyledInput
                   name='source.options.advanced.extBlacklist'
-                  variant='standard'
-                  fullWidth
                   label='Extension blacklist'
                   value={extBlacklist}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
+                  {...inputProps}
                 />
               </Box>
             )
           }
-        ]}/>
+        ]} />
     </>
   )
 }
---
+++ src/pages/DataSources/constants.js
@@ -0,0 +1,31 @@
+export const sourceTypes = {
+  git: {
+    label: 'Git',
+    value: 'git'
+  }
+}
+
+export const gitTypes = {
+  ssh: {
+    label: 'SSH',
+    value: 'ssh'
+  },
+  https: {
+    label: 'HTTPS',
+    value: 'https'
+  },
+}
+
+export const documentLoaders = {
+  TextLoader: {
+    label: 'TextLoader',
+    value: 'text_loader'
+  },
+}
+
+export const extractors = {
+  bert: {
+    label: 'KeyBert',
+    value: 'bert'
+  }
+}
