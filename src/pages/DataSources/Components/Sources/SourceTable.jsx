41ce88cc33de9d5d647bdb120441b39f39cadb73
Jackson Wang <wzx91188@gmail.com>
2024-03-01 12:22:48 +0800

    fix/file-upload-issues (#445)

--- src/pages/DataSources/Components/Sources/SourceTable.jsx
+++ src/pages/DataSources/Components/Sources/SourceTable.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable react/jsx-no-bind */
 import CheckLabel from "@/components/CheckLabel.jsx";
-import NormalRoundButton from "@/components/NormalRoundButton.jsx";
-import { StyledRemoveIcon } from "@/components/SearchBarComponents.jsx";
+import FileUploadControl from "@/components/FileUploadControl";
 import useComponentMode from "@/components/useComponentMode.jsx";
-import { Box, Typography, useTheme } from "@mui/material";
 import { useFormikContext } from "formik";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import FormikInput from "./FormikInput";
 import useOptions from "./useOptions";
 
@@ -20,10 +18,9 @@ const initialState = {
 }
 
 const SourceTable = ({ mode }) => {
-  const theme = useTheme()
   const { isCreate, isView } = useComponentMode(mode)
   const { setFieldValue } = useFormikContext();
-  const options = useOptions({ initialState,  mode });
+  const options = useOptions({ initialState, mode });
 
   const {
     columns = '',
@@ -39,54 +36,15 @@ const SourceTable = ({ mode }) => {
     setFileExt(fileName ? fileName.split('.').at(-1) : '')
   }, [file?.name])
 
-  const fileInput = useRef(null)
-  const handleRemoveFile = () => {
-    fileInput.current.value = ''
-    setFieldValue('source.options.file', initialState.file)
-  }
-  const handleChangeFile = e => {
-    e.target.files.length > 0 &&
-      e.target.files[0] &&
-      setFieldValue('source.options.file', e.target.files[0])
-  }
-
 
   return (
     <>
-      <Box width={'100%'} display={'flex'}
-        sx={{
-          gap: '8px',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          width: '100%',
-          padding: '14px 12px',
-          borderBottom: `1px solid ${theme.palette.border.lines}`,
-        }}
-      >
-        <input type={'file'} hidden ref={fileInput} onChange={handleChangeFile}
-          accept=".csv,.xlsx,application/vnd.ms-excel"
-        />
-        <NormalRoundButton
-          variant='contained'
-          color='secondary'
-          onClick={() => {
-            fileInput.current.click()
-          }}
-          disabled={!isCreate}
-        >
-          Choose file
-        </NormalRoundButton>
-
-        <Typography
-          variant='bodyMedium'
-          component='div'
-          color={isCreate ? theme.palette.text.secondary: theme.palette.text.input.disabled}
-          sx={{ flexGrow: 1 }}
-        >
-          {file?.name}
-        </Typography>
-        { isCreate && <StyledRemoveIcon onClick={handleRemoveFile} /> }
-      </Box>
+      <FileUploadControl
+        file={file}
+        disabled={!isCreate}
+        onChangeFile={(value) => setFieldValue('source.options.file', value)}
+        accept={'.csv,.xlsx,application/vnd.ms-excel'}
+      />
       {fileExt === 'csv' && <FormikInput
         name='source.options.encoding'
         label='File encoding'
