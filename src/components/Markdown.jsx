1fb664353c839d65f0aa0b76f53e7713eee513de
Hawk.Qing <hawk_qing@epam.com>
2024-03-06 10:28:50 +0800

    fix: open markdown links on new tabs

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,7 +1,61 @@
 import { MuiMarkdown, getOverrides } from 'mui-markdown';
 import styled from '@emotion/styled';
-import { MarkdownMapping } from '@/common/constants';
+import Link from '@mui/material/Link';
 
+ const MarkdownMapping = {
+  h1: {
+    component: 'h1',
+    props: {
+    },
+  },
+  h2: {
+    component: 'h2',
+    props: {
+    },
+  },
+  h3: {
+    component: 'h3',
+    props: {
+    },
+  },
+  h4: {
+    component: 'h4',
+    props: {
+    },
+  },
+  h5: {
+    component: 'h5',
+    props: {
+    },
+  },
+  h6: {
+    component: 'h6',
+    props: {
+    },
+  },
+  p: {
+    component: 'p',
+    props: {
+      style: { marginBlockStart: '0px' }
+    },
+  },
+  span: {
+    component: 'span',
+    props: {
+    },
+  },
+  a: {
+    component: Link,
+    props: {
+      target: '_blank'
+    },
+  },
+  li: {
+    component: 'li',
+    props: {
+    },
+  },
+}
 
 const StyledDiv = styled('div')(() => `
   background: transparent;
