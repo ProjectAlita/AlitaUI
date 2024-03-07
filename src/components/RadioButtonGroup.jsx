58e05c426ff8fbd17f1878e951eddfb3a519254f
Jackson Wang <wzx91188@gmail.com>
2024-03-07 16:40:04 +0800

    feat/add-application-type-radio-group (#473)

---
+++ src/components/RadioButtonGroup.jsx
@@ -0,0 +1,48 @@
+import { Box, FormControlLabel, Radio, RadioGroup, Typography, useTheme } from "@mui/material";
+
+export default function RadioButtonGroup({
+  readOnly,
+  value,
+  defaultValue,
+  onChange,
+  items
+}) {
+  const theme = useTheme();
+  return (
+    <RadioGroup
+      aria-labelledby="radio-buttons-group-label"
+      defaultValue={defaultValue}
+      name="radio-buttons-group"
+      value={value}
+      onChange={onChange}
+    >
+      {
+        items.map(item => (<Box key={item.value} display='flex' flexDirection='column'>
+          <FormControlLabel
+            sx={{
+              alignItems: 'flex-start',
+              mb: '8px',
+            }}
+            value={item.value}
+            control={<Radio disabled={readOnly} />}
+            label={
+              <>
+                <Typography 
+                component='div'
+                variant='labelMedium' 
+                color={theme.palette.text.secondary} 
+                sx={{ mt: '7px' }}>
+                  {item.label} 
+                </Typography>
+                {item.description && <Typography component='div' variant='bodySmall'>
+                  {item.description}
+                </Typography>}
+              </>
+            }
+          />
+        </Box>
+        ))
+      }
+    </RadioGroup>
+  );
+}
--- src/pages/Applications/CreateApplication.jsx
+++ src/pages/Applications/CreateApplication.jsx
@@ -1,20 +1,19 @@
+import RocketIcon from '@/components/Icons/RocketIcon';
 import StyledTabs from '@/components/StyledTabs';
 import styled from '@emotion/styled';
 import { Grid } from '@mui/material';
-import * as React from 'react';
-import RocketIcon from '@/components/Icons/RocketIcon';
 import ApplicationCreateForm from "./Components/Applications/ApplicationCreateForm";
 
 const TabContentDiv = styled('div')(({ theme }) => ({
   padding: `${theme.spacing(3)} 0`,
 }))
 
-export default function CreateApplication () {
+export default function CreateApplication() {
   return (
     <Grid container sx={{ padding: '0.5rem 0rem', position: 'fixed', marginTop: '0.7rem' }}>
       <Grid item xs={12}>
         <StyledTabs
-          tabSX={{ paddingX: '24px'}}
+          tabSX={{ paddingX: '24px' }}
           tabs={[{
             label: 'Run',
             icon: <RocketIcon />,
@@ -23,7 +22,10 @@ export default function CreateApplication () {
             content:
               <TabContentDiv>
                 <Grid container sx={{ paddingX: '24px' }}>
-                  <Grid item xs={12} lg={6}>
+                  <Grid item xs={12} lg={6} sx={{
+                    overflowY: 'scroll',
+                    height: 'calc(100vh - 170px)',
+                  }}>
                     <ApplicationCreateForm />
                   </Grid>
                 </Grid>
