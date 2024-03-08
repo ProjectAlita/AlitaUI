088845ad85a0d32bc61a18553969f37b8ac6ede9
Hawk.Qing <HawkQing@users.noreply.github.com>
2024-03-08 10:22:42 +0800

    fix: Correct the font of items on Datasource  page in General block (#477)
    Co-authored-by: Hawk.Qing <hawk_qing@epam.com>

--- src/pages/DataSources/Components/Datasources/EmbeddingModelStorageView.jsx
+++ src/pages/DataSources/Components/Datasources/EmbeddingModelStorageView.jsx
@@ -1,18 +1,20 @@
 import React, { useMemo } from 'react';
 import { Box, Typography } from '@mui/material'
 import { storages } from './DatasourceCreateForm';
+import { useTheme } from '@emotion/react';
 
 const EmbeddingModelStorageView = ({
   embeddingModelName,
   storage,
 }) => {
+  const theme = useTheme();
   const storageName = useMemo(() => storages.find(item => item.value == storage)?.label, [storage])
   return (
     <Box my={1}>
       <Typography variant='bodySmall'>Embedding model: </Typography>
-      <Typography variant='headingSmall'>{embeddingModelName}</Typography>
+      <Typography color={theme.palette.text.secondary} variant='bodySmall'>{embeddingModelName}</Typography>
       <Typography variant='bodySmall' ml={2}>Storage: </Typography>
-      <Typography variant='headingSmall'>{storageName}</Typography>
+      <Typography color={theme.palette.text.secondary} variant='bodySmall'>{storageName}</Typography>
     </Box>
   );
 }
